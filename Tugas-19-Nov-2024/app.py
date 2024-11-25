66f52a6fcf1d40bc4a65abacb09b22265e9039c0
geralnb <shirometeora@gmail.com>
2024-11-25 21:51:20 +0700

    Fix command injection vulnerability

--- Tugas-19-Nov-2024/app.py
+++ Tugas-19-Nov-2024/app.py
@@ -1,9 +1,11 @@
 import subprocess
+import shlex
 
 def greet(name):
     print(f"Hello, {name}!")
 
 def run_command(cmd):
+    safe_cmd = shlex.split(cmd)
     subprocess.call(cmd, shell=True)
 
 if __name__ == "__main__":
