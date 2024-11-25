a2ee59c8094822ff7d051502e62b342d605e3ab9
geralnb <shirometeora@gmail.com>
2024-11-25 22:41:05 +0700

    delete shell=True

--- Tugas-19-Nov-2024/app.py
+++ Tugas-19-Nov-2024/app.py
@@ -6,7 +6,7 @@ def greet(name):
 
 def run_command(cmd):
     safe_cmd = shlex.split(cmd)
-    subprocess.call(cmd, shell=True)
+    subprocess.call(cmd)
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
